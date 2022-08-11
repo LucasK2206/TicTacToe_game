75aa1a5dc560728a600d11fdecec0187b56204f8
Lukasz <bleyosss@wp.pl>
2022-08-11 21:16:29 +0200

    app ended alternatively for minor corrections

--- tictactoe_app.js
+++ tictactoe_app.js
@@ -1,31 +1,75 @@
 const gameBoard = document.querySelector("#game-board");
 const allBoardBoxes = [...document.querySelectorAll(".game__board--box")];
-const winnerBox = document.querySelector(".game__winnerboard--winner");
+const winnerBox = document.querySelector("#game__winnerboard--winner");
 const resetBtn = document.querySelector("#game__winnerboard--reset");
+const winnerBoard = document.querySelector("#game__winnerboard");
 
 const playerX = "X";
 const playerO = "O";
 let actualPlayer = null;
 
-function handleClick(){
-    this.classList.add(actualPlayer);
-}
-
-function addEventListeners() {
-    allBoardBoxes.forEach(box => {
-        box.addEventListener("click", handleClick);
-    })
-}
-function pickPlayer() {
-    return actualPlayer = (Math.floor(Math.random()*20) % 2) === 0 ? playerX : playerO
-}
-function gameInit() {
-    pickPlayer();
-    gameBoard.classList.add(actualPlayer);
-    addEventListeners();
-}
 
 document.addEventListener("DOMContentLoaded", () => {
     gameInit();
 })
 
+const gameInit = function() {
+    pickPlayer();
+    gameBoard.classList.add(actualPlayer);
+    takeBoxes();
+}
+
+const pickPlayer = function() {
+    return actualPlayer = (Math.floor(Math.random()*20) % 2) === 0 ? playerX : playerO
+}
+
+const takeBoxes = function() {
+    allBoardBoxes.forEach(box => {
+        box.addEventListener("click", handleClick);
+    })
+}
+
+const winningCombinations = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+];
+
+const checkWinningCombinations = function(actualPlayer) {
+    return winningCombinations.some(combinations => {
+        return combinations.every(combination => {
+            return allBoardBoxes[combination].classList.contains(actualPlayer)
+        })
+    })
+}
+
+const isDraw = function(){
+    return allBoardBoxes.every(box => {
+        return box.classList.contains(playerX) || box.classList.contains(playerO)
+    })
+}
+const resetBtnClicked = function() {
+    location.reload();
+}
+
+const handleClick = function(){
+    this.classList.add(actualPlayer);
+    gameBoard.classList.remove(actualPlayer);
+    if(checkWinningCombinations(actualPlayer) === true){
+        winnerBoard.classList.remove("game__winnerboard--disabled");
+        winnerBoard.classList.add("game__winnerboard")
+        winnerBox.innerHTML = `Wygrał: ${actualPlayer}`;
+        resetBtn.addEventListener("click", resetBtnClicked)
+    }
+    else if (isDraw()){
+        winnerBoard.classList.remove("game__winnerboard--disabled");
+        winnerBoard.classList.add("game__winnerboard")
+        winnerBox.innerHTML = `Remis`;
+        resetBtn.addEventListener("click", resetBtnClicked)
+    }
+
+
+    this.removeEventListener("click", handleClick)
+    actualPlayer = (actualPlayer === playerX ? playerO : playerX);
+    gameBoard.classList.add(actualPlayer);
+}
